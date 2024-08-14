f258915b8e22bbee7cc5bc020dac975b27f325a0
Korey Arnold <koreyarnold4@gmail.com>
2024-08-14 11:44:38 -0400

    done

--- intervals/script.js
+++ intervals/script.js
@@ -1,5 +1,17 @@
-// number of seconds that have passed
+// Number of seconds that have passed
 let time = 0;
-// reference to the <span> containing the number
+
+// Reference to the <span> element containing the number of seconds
 let counterElement = document.getElementById("time");
 
+// Function to update the counter every second
+function updateCounter() {
+    // Increment the time variable by 1
+    time++;
+    // Update the text content of the counterElement to reflect the new time
+    counterElement.textContent = time;
+}
+
+// Use setInterval to call updateCounter every 1000 milliseconds (1 second)
+setInterval(updateCounter, 1000);
+//I asked gpt for questions and add comments to better understand what each line of code do.
